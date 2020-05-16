035065e064f1123e5e5ec7e20af4d3b4dcd17c3c
Ben Chatelain <ben@octop.ad>
2020-05-16 15:40:13 -0600

    ➕ ArgumentParser (0.0.6)

--- Package.swift
+++ Package.swift
@@ -6,15 +6,14 @@ import PackageDescription
 let package = Package(
     name: "signpass",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "signpass",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]),
         .testTarget(
             name: "signpassTests",
             dependencies: ["signpass"]),
--- Sources/signpass/main.swift
+++ Sources/signpass/main.swift
@@ -1,1 +1,11 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct Random: ParsableCommand {
+    @Argument() var highValue: Int
+
+    func run() {
+        print(Int.random(in: 1...highValue))
+    }
+}
+
+Random.main()
