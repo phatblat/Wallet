b897a5c517e956272b2ec6f8b5a3286283719a9b
Ben Chatelain <ben@octop.ad>
2020-05-17 19:07:52 -0600

    👽 Set platform to macOS 10.15

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "signpass",
+    platforms: [
+        .macOS(.v10_15),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
     ],
