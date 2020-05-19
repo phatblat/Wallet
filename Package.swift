879f123d21a26b60ed4493300cf1a20e87136976
Ben Chatelain <ben@octop.ad>
2020-05-18 22:54:44 -0600

    ➕ swift-crypto (1.0.1)

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
             targets: ["WalletKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "0.0.6")),
+        .package(url: "https://github.com/apple/swift-crypto", .upToNextMajor(from: "1.0.1")),
         .package(url: "https://github.com/Quick/Quick", .upToNextMajor(from: "2.2.0")),
         .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "8.0.9")),
     ],
@@ -29,7 +30,8 @@ let package = Package(
         .target(
             name: "WalletKit",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Crypto", package: "swift-crypto")
             ]),
         .testTarget(
             name: "WalletKitTests",
